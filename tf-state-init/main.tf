80287e561d4b8fd5b0f3c75d3ed273c49460dd10
Nishchitha A <nischiashok0@gmail.com>
2025-08-24 16:07:50 +0530

    demo

--- tf-state-init/main.tf
+++ tf-state-init/main.tf
@@ -1,3 +1,8 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "e0be8e24-25e7-4901-ad14-ea389c0f1289"
+}
+
 resource "azurerm_storage_account" "example" {
   name                     = "tfstate"
   resource_group_name      = "project-setup-1"
