178a718a4b9efe5f5160b4c57f4048f3717fa7f8
Nishchitha A <nischiashok0@gmail.com>
2025-08-30 12:02:54 +0530

    added

---
+++ remote/main.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "azurerm" {
+    use_cli              = true
+    #tenant_id            = "00000000-0000-0000-0000-000000000000"
+    subscription_id      = " e0be8e24-25e7-4901-ad14-ea389c0f1289"
+    resource_group_name  = "project-setup-1"
+    storage_account_name = "devtfstates"
+    container_name       = "roboshop-state-files"
+    key                  = "test.terraform.tfstate"
+  }
+}
