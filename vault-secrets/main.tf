bf463b2cb53977f4e03e163c221efce9d9497410
Nishchitha A <nischiashok0@gmail.com>
2025-09-03 21:07:27 +0530

    added

--- vault-secrets/main.tf
+++ vault-secrets/main.tf
@@ -6,6 +6,7 @@ resource "vault_mount" "kv" {
   description = each.value["description"]
 }
 resource "vault_kv_secret_v2" "values" {
+  depends_on = [vault_mount.kv]
   for_each   = var.values
   mount      = each.value["secret"]
   name       = each.key
