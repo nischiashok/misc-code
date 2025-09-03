85173693937e09638d7938d5994dc5e49c6b7d2b
Nishchitha A <nischiashok0@gmail.com>
2025-09-03 15:11:23 +0530

    added

--- tf-state-init/main.tf
+++ tf-state-init/main.tf
@@ -3,16 +3,16 @@ provider "azurerm" {
   subscription_id = "e0be8e24-25e7-4901-ad14-ea389c0f1289"
 }
 
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate"
+resource "azurerm_storage_account" "example" {
+  name                     = "devopstfstate"
   resource_group_name      = "project-setup-1"
   location                 = "UK West"
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
-#
-# resource "azurerm_storage_container" "tfstates" {
-#   name                  = "roboshop-state-files"
-#   storage_account_id    = azurerm_storage_account.tfstate.id
-#   container_access_type = "private"
-# }
+
+resource "azurerm_storage_container" "tfstates" {
+  name                  = "roboshop-state-files"
+  storage_account_id    = azurerm_storage_account.tfstate.id
+  container_access_type = "private"
+}
