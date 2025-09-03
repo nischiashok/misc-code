cea8649661878522b9662fae9726d3cc655e6226
Nishchitha A <nischiashok0@gmail.com>
2025-09-03 15:05:58 +0530

    added

--- tf-state-init/main.tf
+++ tf-state-init/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "devopstfstate"
+  name                     = "storagetfstate"
   resource_group_name      = "project-setup-1"
   location                 = "UK West"
   account_tier             = "Standard"
