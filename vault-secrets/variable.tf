a36a15db8b0c67108d87e92e7e509884aaefebd9
Nishchitha A <nischiashok0@gmail.com>
2025-09-03 21:48:03 +0530

    added

--- vault-secrets/variable.tf
+++ vault-secrets/variable.tf
@@ -20,6 +20,17 @@ variable "values" {
         foo = "bar"
       }
     }
+
+    frontend={
+      secret = "roboshop-dev"
+      value = {
+        catalogue_url = "https://catalogue-dev.omshiva.shop:8080"
+        user_url = "https://user-dev.omshiva.shop:8080"
+        cart_url = "https://cart-dev.omshiva.shop:8080"
+        payment_url = "https://payment-dev.omshiva.shop:8080"
+        shipping_url = "https://shipping-dev.omshiva.shop:8080"
+      }
+    }
     ssh = {
       secret = "roboshop-infra"
       value ={
