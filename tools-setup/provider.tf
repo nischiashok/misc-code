486123ee4c4020fe955434f76fbadcc8bfd3f4f9
Nishchitha A <nischiashok0@gmail.com>
2025-09-03 15:44:16 +0530

    added

--- tools-setup/provider.tf
+++ tools-setup/provider.tf
@@ -7,7 +7,7 @@ terraform {
   backend "azurerm" {
     subscription_id      = "e0be8e24-25e7-4901-ad14-ea389c0f1289"
     resource_group_name  = "project-setup-1"
-    storage_account_name = "devopstfstate"
+    storage_account_name = "nishiashoksatfstate"
     container_name       = "roboshop-state-files"
     key                  = "tools.terraform.tfstate"
   }
