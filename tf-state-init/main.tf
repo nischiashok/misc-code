3ead0f75337cc3fd6a15fd1301bc3f3ebe78d723
Nishchitha A <nischiashok0@gmail.com>
2025-09-03 15:19:59 +0530

    added

--- tf-state-init/main.tf
+++ tf-state-init/main.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "NishchithaAshokSAtf-state"
+  name                     = "nishiashoksatf-state"
   resource_group_name      = "project-setup-1"
   location                 = "UK West"
   account_tier             = "Standard"
