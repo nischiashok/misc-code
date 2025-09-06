3f18fac534baa36472c7afb3e5578ba48a0006b2
Nishchitha A <nischiashok0@gmail.com>
2025-09-06 15:03:47 +0530

    Changes in main.tf of vault-secrete

--- vault-secrets/main.tf
+++ vault-secrets/main.tf
@@ -1,3 +1,17 @@
+# resource "vault_mount" "kv" {
+#   for_each    = var.secrets
+#   path        = each.key
+#   type        = "kv"
+#   options     = { version = "2" }
+#   description = each.value["description"]
+# }
+# resource "vault_kv_secret_v2" "values" {
+#   depends_on = [vault_mount.kv]
+#   for_each   = var.values
+#   mount      = each.value["secret"]
+#   name       = each.key
+#   data_json  = jsonencode(each.value["value"])
+# }
 resource "vault_mount" "kv" {
   for_each    = var.secrets
   path        = each.key
@@ -5,6 +19,7 @@ resource "vault_mount" "kv" {
   options     = { version = "2" }
   description = each.value["description"]
 }
+
 resource "vault_kv_secret_v2" "values" {
   depends_on = [vault_mount.kv]
   for_each   = var.values
@@ -12,3 +27,5 @@ resource "vault_kv_secret_v2" "values" {
   name       = each.key
   data_json  = jsonencode(each.value["value"])
 }
+
+
