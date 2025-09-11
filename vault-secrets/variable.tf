0123ca744ff96b4c896be7d2532770c1cef189b8
Nishchitha A <nischiashok0@gmail.com>
2025-09-11 20:15:11 +0530

    demo

--- vault-secrets/variable.tf
+++ vault-secrets/variable.tf
@@ -27,9 +27,6 @@ variable "values" {
       value = {
         MONGO       = "true"
         MONGO_URL   = "mongodb://mongodb-dev.omshiva.shop:27017/catalogue"
-        DB_TYPE     = "mongo"
-        APP_GIT_URL = "https://github.com/roboshop-devops-project-v3/catalogue"
-        DB_HOST     = "mongodb-dev.omshiva.shop"
       }
     }
 
@@ -47,21 +44,17 @@ variable "values" {
       value = {
         CART_ENDPOINT = "cart:8080"
         DB_HOST       = "mysql-dev.omshiva.shop"
-        DB_USER       = "root"
-        DB_PASS       = "RoboShop@1"
         username      = "root"
         password      = "RoboShop@1"
-        DB_TYPE       = "mysql"
-        APP_GIT_URL   = "https://github.com/roboshop-devops-project-v3/shipping"
       }
     }
 
     payment = {
       secret = "roboshop-dev"
       value = {
-        CART_HOST = "cart"
+        CART_HOST = "cart-dev.omshiva.shop"
         CART_PORT = "8080"
-        USER_HOST = "user"
+        USER_HOST = "user-dev.omshiva.shop"
         USER_PORT = "8080"
         AMQP_HOST = "rabbitmq-dev.omshiva.shop"
         AMQP_USER = "roboshop"
@@ -77,18 +70,6 @@ variable "values" {
         cart_url       = "http://cart-dev.omshiva.shop:8080/"
         payment_url    = "http://payment-dev.omshiva.shop:8080/"
         shipping_url   = "http://shipping-dev.omshiva.shop:8080/"
-        CATALOGUE_HOST = "catalogue"
-        CATALOGUE_PORT = 8080
-        USER_HOST      = "user"
-        USER_PORT      = 8080
-        CART_HOST      = "cart"
-        CART_PORT      = 8080
-        SHIPPING_HOST  = "shipping"
-        SHIPPING_PORT  = 8080
-        PAYMENT_HOST   = "payment"
-        PAYMENT_PORT   = 8080
-
-
       }
     }
 
